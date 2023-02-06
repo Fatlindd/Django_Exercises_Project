618f8106352da383d8fcf8efa6b239344209f21d
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-06 13:44:03 +0100

    I import departments.models to use Department model for Student model because one departments have more than one student.

--- projects/university/students/models.py
+++ projects/university/students/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from projects.university.departments.models import Department
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
-    
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
