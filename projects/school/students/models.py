be43cdc731eed82d7c38d4d401ec58c50ee73bb9
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-07 11:36:20 +0100

    I create two models of my apps, that generate a table in database, and save literals of variables in fields of tables.

--- projects/school/students/models.py
+++ projects/school/students/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from courses.models import Course
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
