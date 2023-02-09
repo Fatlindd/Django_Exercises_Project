ccacdb45360e1d23ac315e0dd8e5ce03b9bff45a
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-09 15:29:38 +0100

    Some tasks in Python Shell to get data from database table we created in our models in project apps.

--- projects/university/students/models.py
+++ projects/university/students/models.py
@@ -6,3 +6,6 @@ class Student(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
