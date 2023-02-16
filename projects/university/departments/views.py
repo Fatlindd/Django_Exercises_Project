27751ee5b8d6a5885fd1e7152128dfc292bc2d03
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-16 14:55:39 +0100

    Adding new view named detail() and map it to a url.

--- projects/university/departments/views.py
+++ projects/university/departments/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to our University.")
+
+
+def detail(request, pk):
+    return HttpResponse(f"Details of department with id {pk}")
+
