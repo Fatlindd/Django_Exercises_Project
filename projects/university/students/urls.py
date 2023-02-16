2c34c3bba9e7ed54251b258351d3a2e79c9e6cbe
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-16 15:06:10 +0100

    Adding new view named detail() to Student Model and map it to a url.

--- projects/university/students/urls.py
+++ projects/university/students/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('<int:pk>/', views.detail, name='detail'),
+    
 ]
--- projects/university/students/views.py
+++ projects/university/students/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Students.")
+
+
+def detail(request, pk):
+    return HttpResponse(f"Details of student with id {pk}")
