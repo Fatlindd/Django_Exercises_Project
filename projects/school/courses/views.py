92fd1fc08ae1b4f9b4472f6a7b9a810f56aeb342
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-23 14:34:20 +0100

    Adding new view detail, and modify course detail view so it returns details of course with id pk.

--- projects/school/courses/views.py
+++ projects/school/courses/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from .models import Course
 
 
 def home(request):
-    return HttpResponse("Welcome to our School.")
+    courses = Course.objects.all()
+    output = ''.join([f'<p>{course.name}</p>' for course in courses])
+    return HttpResponse(output)
+
+
+def detail(request, pk):
+    course = Course.objects.get(pk=pk)
+    students = course.student_set.all()
+    students = ''.join([f'{student.first_name} {student.last_name} ' for student in students])
+    output = f'<i>Name: </i> <b>{course.name}</b><br><i>Students:</i> <b>{students}</b>'
+    return HttpResponse(output)
