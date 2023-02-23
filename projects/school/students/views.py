bf23ea894bd4578ddd6b74f591319bd8a3daa5cc
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-23 14:54:28 +0100

    Adding new view detail to student,  so it returns details of student with id pk.

--- projects/school/students/views.py
+++ projects/school/students/views.py
@@ -7,3 +7,8 @@ def home(request):
     output = '<br>'.join([f'{student.full_name()}' for student in students])
     return HttpResponse(output)
 
+
+def detail(request, pk):
+    student = Student.objects.get(pk=pk)
+    s = '<i>Name:</i> {}<br><i>Age:</i>{}<br><i>Course: {}</i>'
+    return HttpResponse(s.format(student.full_name(), student.age, student.course))
