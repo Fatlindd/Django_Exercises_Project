cb9f6a094e43150752f33b2a6ae08b4aeda8c668
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-23 14:42:06 +0100

    Modify students home view so it returns a list of all students on a new line, and also adding new methot on Student model, to return full name instead of returning in seperate way firstname and lastname.

--- projects/school/students/models.py
+++ projects/school/students/models.py
@@ -10,3 +10,6 @@ class Student(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
--- projects/school/students/views.py
+++ projects/school/students/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from .models import Student
 
 
 def home(request):
-    return HttpResponse("Students.")
+    students = Student.objects.all()
+    output = '<br>'.join([f'{student.full_name()}' for student in students])
+    return HttpResponse(output)
+
