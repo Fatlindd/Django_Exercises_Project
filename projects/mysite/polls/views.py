ab1fc2c1d004d8b4a973bb4b708704f8aaa11ff3
Fatlind <fatlind.thaqi98@gmail.com>
2023-02-24 14:25:08 +0100

    Adding some templates to  new file index.html on directory templates/polls

--- projects/mysite/polls/views.py
+++ projects/mysite/polls/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    output = template.render(context, request)
     return HttpResponse(output)
 
 
